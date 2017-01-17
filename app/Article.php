83c59b75505047f6979b9080927794287a4c7ea2
liushuai007 <271606018@qq.com>
2017-01-17 15:37:41 +0800

    修改article route类

--- app/Article.php
+++ app/Article.php
@@ -11,17 +11,4 @@ class Article extends Model
     protected $primaryKey = 'id';
 
     protected $fillable = ['title', 'content', 'author', 'created_at', 'updated_at'];
-
-    public static function create()
-    {
-        $art = new Article();
-
-        $art->title = '小钞钞';
-        $art->content = '小钞钞不算太傻';
-        $art->author = '小钞钞';
-
-        $res = $art->save();
-
-        var_dump($res);
-    }
 }
