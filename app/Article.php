f032537eb3d88fb6db6c59ca8a45e6fd0a020d8c
liushuai007 <271606018@qq.com>
2017-01-17 10:11:25 +0800

    添加article model类

---
+++ app/Article.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+
+class Article extends Model
+{
+    protected $table = 'articles';
+    protected $primaryKey = 'id';
+}
